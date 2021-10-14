918440d35f37781d6e4442b2e1e12e5bdac0f8fd
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-10-14 17:38:33 -0300

    back-end

---
+++ back-end/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8f0d62fe3264401219c386452ae43be1'),
+    },
+  },
+});
---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '332387d1-c8de-4995-a40e-782969b1f936'
+};
