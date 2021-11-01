21c87d55fe839cfde9abdb8bbeeaba559b0c41ca
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-10-31 22:12:19 -0300

    slog

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -12,14 +12,16 @@ export default function Home(props) {
       <h1> Todos os posts </h1>
       {props.post.map((item) => (
         <div>
-          <div key={item.id} className={styles.postsHome}>
-            <h2>{item.Titulo}</h2>
-            <p>{item.Resumo}</p>
-            <span>{item.Data}</span>
-            <div>
-              <img src={item.imagem.name} alt={item.Titulo}/>
+          <a href={`/post/${item.slog}`}>
+            <div key={item.id} className={styles.postsHome}>
+              <h2>{item.Titulo}</h2>
+              <p>{item.Resumo}</p>
+              <span>{item.Data}</span>
+              <div>
+                <img src={item.imagem.name} alt={item.Titulo}/>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       ))}
     </section>
