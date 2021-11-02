ac6b52421709463951daed6aff16006ebdfdc051
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-11-01 23:04:47 -0300

    api posts

--- front-end/pages/api/posts/[id].js
+++ front-end/pages/api/posts/[id].js
@@ -1,9 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// export default function handler(req, res) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
-
 export default async (req, res) => {
     const result = await fetch(`http://localhost:1337/posts/${req.query.id}`)
     const json = await result.json()
@@ -11,4 +5,4 @@ export default async (req, res) => {
     res.status(200).json({ 
       info: json
     })
-  }
+}
