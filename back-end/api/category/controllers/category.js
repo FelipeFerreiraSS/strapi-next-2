9cda09609b0bc51b9ef4cc73aeb7b06e6244e2f9
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-11-07 20:31:02 -0300

    categories slug [error]

--- back-end/api/category/controllers/category.js
+++ back-end/api/category/controllers/category.js
@@ -6,3 +6,21 @@
  */
 
 module.exports = {};
+
+// const { sanitizeEntity } = require('strapi-utils');
+
+// module.exports = {
+//   /**
+//    * Retrieve a record.
+//    *
+//    * @return {Object}
+//    */
+
+//   async findOne(ctx) {
+//     const { slug } = ctx.params;
+
+//     const entity = await strapi.services.categories.findOne({ slug });
+//     return sanitizeEntity(entity, { model: strapi.models.categories });
+//   },
+// };
+
